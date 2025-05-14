09e88b0e906c7b35b00d09c85ce41e8e5f502534
Dinu Dassanayake <76268562+dinu-dassanayake@users.noreply.github.com>
2025-05-14 10:09:41 -0400

    base game iteration #2

--- FlappyBird.java
+++ FlappyBird.java
@@ -3,6 +3,30 @@ import java.util.*;
 import javax.swing.*;
 public class FlappyBird {
     public static void main(String[] args) {
+        int width = 360;
+        int length = 640;
 
+        Image backgroundImg;
+        Image birdImg;
+        Image topPipeImg;
+        Image bottomPipeImg;
+
+        int birdX = boardWidth/8;
+        int birdY = boardHeight/2;
+        int birdWidth = 34;
+        int birdHeight = 24;
+
+        class Bird {
+            int x = birdX;
+            int y = birdY;
+            int width = birdWidth;
+            int height = birdHeight;
+            Image img;
+
+            Bird(Image img) {
+                this.img = img;
+            }
+            
+        }
     }
 }
